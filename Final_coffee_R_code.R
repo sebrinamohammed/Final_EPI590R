260a845dfecf94bc45f58aeeb77b8da0f548e6f2
Sebrina Mohammed <sebrinamohammed@Sebrinas-MacBook-Air.local>
2024-08-19 15:38:38 -0400

    just working

--- Final_coffee_R_code.R
+++ Final_coffee_R_code.R
@@ -4,3 +4,15 @@ library(gtsummary)
 library(readr)
 coffee_survey <- read_csv("coffee_survey.csv")
 View(coffee_survey)
+
+tbl_summary(
+	coffee_survey,
+	by = roast_level,
+	include = c(caffeine, style, strength,
+							total_spend, spent_equipment,wfh,gender, prefer_abc, prefer_ad, prefer_overall))
+#label = list()
+#statistic=list()
+#digits=listis()
+
+
+

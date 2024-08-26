423d2b9e46dd64ee00fd3579871821703952a0a7
Sebrina Mohammed <sebrinamohammed@Sebrinas-MacBook-Air.local>
2024-08-26 11:44:01 -0400

    monday

--- Final_coffee_R_code.R
+++ Final_coffee_R_code.R
@@ -23,19 +23,16 @@ tbl_summary(
 	coffee_survey,
 	by = age,
 	include = c(
-		cups,where_drink, brew, purchase,favorite, additions,
-		dairy, sweetener, strength, style, roast_level, caffeine, expertise, coffee_a_bitter,
-		coffee_a_acid,coffee_a_pref, coffee_b_bitter, coffee_b_acid,
-		coffee_b_pref, coffee_c_bitter, coffee_c_acid, coffee_c_pref, coffee_c_bitter, coffee_d_acid,
-		coffee_d_pref, prefer_abc,	prefer_ad, prefer_overall, wfh,	total_spend,	why_drink,	taste, know_source, most_paid,
-		most_willing, value_cafe,	spent_equipment,	value_equipment,	gender, education_level,
-		ethnicity_race, employment_status,	number_children, political_affiliation
-		)
+		employment_status, strength, caffeine, gender
 	)
+)
 ########Fit a regression and present well-formatted results from the regression (1 pt)
 ####The regression doesn’t have to be of any particular scientific interest, and you don’t have to interpret it in any particular way
 ####You may use {broom} or {gtsummary} or both
 ###IDK
+
+
+
 tbl_uvregression(
 	coffee_survey,
 	y = strength,
